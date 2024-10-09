2b3cef307e883f22f304a1e34e88a628f1aaf20a
Omar-CcL <U22227800@utp.edu.pe>
2024-10-09 01:11:20 -0500

    Cambios en html y conexion a la db

---
+++ src/java/modelo/DatabaseConnection.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private DataSource dataSource;
+
+    private DatabaseConnection() {
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env");
+            dataSource = (DataSource) envContext.lookup("jdbc/ClinicaVeterinaria");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+    
+    //Para la conexion
+    //Connection conn = DatabaseConnection.getInstance().getConnection();
+}
