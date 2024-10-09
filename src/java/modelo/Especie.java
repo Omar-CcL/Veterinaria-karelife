4b89c62191d46b455a505136b27ce79c8fbfdc1d
Omar-CcL <U22227800@utp.edu.pe>
2024-10-09 02:48:28 -0500

    actu java

--- src/java/modelo/Especie.java
+++ src/java/modelo/Especie.java
@@ -1,27 +1,3 @@
-public abstract class AnimalPrototype implements Cloneable {
-    protected String nombre;
-    protected String estado;
-
-    @
-    public abstract AnimalPrototype clone();
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-}
-
 // Especie concreta
 public class Especie extends AnimalPrototype {
 
@@ -36,16 +12,3 @@ public class Especie extends AnimalPrototype {
     }
 }
 
-// Raza concreta
-public class Raza extends AnimalPrototype {
-
-    public Raza(String nombre, String estado) {
-        this.nombre = nombre;
-        this.estado = estado;
-    }
-
-    @Override
-    public AnimalPrototype clone() {
-        return new Raza(this.nombre, this.estado);
-    }
-}
