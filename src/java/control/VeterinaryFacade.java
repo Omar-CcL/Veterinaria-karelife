336d38e4d06708a20e20424c6753c183eb74f2fa
Omar-CcL <U22227800@utp.edu.pe>
2024-10-09 02:39:05 -0500

    actualización grande

---
+++ src/java/control/VeterinaryFacade.java
@@ -0,0 +1,19 @@
+package java.control;
+
+public class VeterinaryFacade {
+    private CitaService citaService;
+    private ClienteService clienteService;
+    private MascotaService mascotaService;
+
+    public VeterinaryFacade() {
+        this.citaService = new CitaService();
+        this.clienteService = new ClienteService();
+        this.mascotaService = new MascotaService();
+    }
+
+    public void agendarCita(int clienteId, int mascotaId, int servicioId, LocalDateTime fechaCita) {
+        Cliente cliente = clienteService.getClienteById(clienteId);
+        Mascota mascota = mascotaService.getMascotaById(mascotaId);
+        citaService.agendarCita(cliente, mascota, servicioId, fechaCita);
+    }
+}
---
+++ src/java/modelo/Especie.java
@@ -0,0 +1,51 @@
+public abstract class AnimalPrototype implements Cloneable {
+    protected String nombre;
+    protected String estado;
+
+    @
+    public abstract AnimalPrototype clone();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
+
+// Especie concreta
+public class Especie extends AnimalPrototype {
+
+    public Especie(String nombre, String estado) {
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    @Override
+    public AnimalPrototype clone() {
+        return new Especie(this.nombre, this.estado);
+    }
+}
+
+// Raza concreta
+public class Raza extends AnimalPrototype {
+
+    public Raza(String nombre, String estado) {
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    @Override
+    public AnimalPrototype clone() {
+        return new Raza(this.nombre, this.estado);
+    }
+}
